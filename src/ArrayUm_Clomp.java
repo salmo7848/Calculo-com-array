fb4e19722d8099ea5e11aefaaadf24f354725ddb
Salmo Alves <salmo7848@outlook.com>
2021-03-25 07:51:09 -0300

    Alteração nas formulas em ArrayUm_Clomp, adicionado comentários

--- src/ArrayUm_Clomp.java
+++ src/ArrayUm_Clomp.java
@@ -1,116 +1,61 @@
-
-
-import java.util.Scanner;
-
-public class ArrayUm_Clomp {
-
-	Scanner sc = new Scanner(System.in);
 	
-	int[] dados = new int[5];
 	
-	public int vetores() {
-		
-		
-		
-		dados[0]=sc.nextInt();
-	    dados[1]=sc.nextInt();
-	    dados[2]=sc.nextInt();
-	    dados[3]=sc.nextInt();
-	    dados[4]=sc.nextInt();
-	    
-	    return 0;
-	}
+	import java.util.Scanner;
 	
-	public int soma() {
-		String resp;
-		do {
-
+	public class ArrayUm_Clomp {
+		Scanner sc = new Scanner(System.in);
+		
+	    public int vetores() {
+	    	int i;
+	    	System.out.println("informe o valor: ");
+			int n = sc.nextInt();
+			
+		
+		int[] dados = new int[n];
+		int soma=0;
+		/*LINHAS: 19 á 29
+		 * Bloco abaixo informa a posição do vetor em que cada numero
+		 * informado pelo usuario se encontra*/
+	    	for(i=0; i<n; i++) {
+	    		
+	    		System.out.println("Informe o numero: ");
+	    		dados[i]=sc.nextInt();
+	    		
+	    		System.out.println("posição de "+ dados[i]+" é  "+i);
+	    		
+	    		
+	    		System.out.println(dados[i]);
+	    		
+	    	}
+	    	/*LINHAS: 32 á 40
+	    	 * Bloco abaixo informa o numero  de cada posição informada pelo usuario*/
+			String resp;
+			do{
+			sc.nextLine();	
+			System.out.print("\nSelecione as posições : ");
+			i = sc.nextInt();
+			
+			System.out.println("Valor de posição "+i+" é: "+dados[i]);
+			
+			soma+=dados[i];
+			/*LINHAS: 46 á 52
+			 * O usuario digitando S será solicitado que o mesmo escolha 
+			 * mais uma opção do vetor
+			 * Caso digite N, sairá do laço e mostrará a soma dos vetores
+			 * selecionados*/
 			sc.nextLine();
-			System.out.print("Informe a opção: ");
-			String opcao = sc.nextLine();
-			
-			switch(opcao) {
-			case "a":
-				int soma = dados[0]+dados[1]+dados[2]+dados[3]+dados[4];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-				
-			case "b":
-			    soma = dados[0]+dados[1]+dados[2]+dados[3];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-				
-			case "c":
-			    soma = dados[0]+dados[1]+dados[2];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-				
-			case "d":
-			    soma = dados[0]+dados[1];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-			case "e":
-			    soma = dados[0]+dados[1]+dados[3];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-			case "f":
-			    soma = dados[0]+dados[1]+dados[4];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-			case "g":
-			    soma = dados[1]+dados[2]+dados[3]+dados[4];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-			case "h":
-			    soma = dados[1]+dados[2]+dados[3];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-			case "i":
-			    soma = dados[1]+dados[2];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-			case "j":
-			    soma = dados[1]+dados[3];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-				
-			case "k":
-			    soma = dados[1]+dados[4];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-				
-			case "l":
-			    soma = dados[2]+dados[3];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-				
-			case "m":
-			    soma = dados[2]+dados[4];	
-				System.out.println("Valor da soma: "+soma);
-				
-				break;
-			
-			}
-			System.out.print("Deseja continuar: ");
+			System.out.print("\nDeseja continuar S/N:  ");
 			resp = sc.nextLine();
-			}while(resp!="S");
-
-	    sc.close();
-		return soma();
+			}while(!resp.equals("N") && !resp.equals("n"));
+			
+			
+			System.out.println("soma dos vetores selecione "+soma);
+			sc.nextLine();
+			return vetores();
 		
+		    
+		}
+	    
 	}
-	
-}
+
 
